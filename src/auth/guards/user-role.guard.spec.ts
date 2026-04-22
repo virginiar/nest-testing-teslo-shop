87e530779cd9cccc87f628e82537ccfaa767a598
Virginia Redondo <7237305+virginiar@users.noreply.github.com>
2026-04-22 11:56:23 +0200

    Añadiendo pruebas unitarias en UserGuardRole de manejo de validación de roles y excepciones

--- src/auth/guards/user-role.guard.spec.ts
+++ src/auth/guards/user-role.guard.spec.ts
@@ -1,6 +1,10 @@
 import { Reflector } from '@nestjs/core';
 import { UserRoleGuard } from './user-role.guard';
-import { ExecutionContext } from '@nestjs/common';
+import {
+  BadRequestException,
+  ExecutionContext,
+  ForbiddenException,
+} from '@nestjs/common';
 
 describe('UserRole Guard', () => {
   let guard: UserRoleGuard;
@@ -29,5 +33,44 @@ describe('UserRole Guard', () => {
     jest.spyOn(reflector, 'get').mockReturnValue([]);
 
     expect(guard.canActivate(mockContext)).toBe(true);
+  });
+
+  it('should throw BadRequestException if user is not found', () => {
+    jest.spyOn(reflector, 'get').mockReturnValue(['admin']);
+    jest.spyOn(mockContext.switchToHttp(), 'getRequest').mockReturnValue({});
+
+    expect(() => guard.canActivate(mockContext)).toThrow(BadRequestException);
+    expect(() => guard.canActivate(mockContext)).toThrow('User not found');
+  });
+
+  it('should return true if user has a valid role', () => {
+    jest.spyOn(reflector, 'get').mockReturnValue(['admin']);
+    jest.spyOn(mockContext.switchToHttp(), 'getRequest').mockReturnValue({
+      user: {
+        roles: ['admin'],
+        fullName: 'Fernando',
+      },
+    });
+
+    expect(guard.canActivate(mockContext)).toBe(true);
+  });
+
+  it('should throw ForbiddenException if user lacks required role', () => {
+    const user = {
+      roles: ['user'],
+      fullName: 'Fernando Herrera',
+    };
+
+    const validRoles = ['admin'];
+
+    jest.spyOn(reflector, 'get').mockReturnValue(validRoles);
+    jest.spyOn(mockContext.switchToHttp(), 'getRequest').mockReturnValue({
+      user: user,
+    });
+
+    expect(() => guard.canActivate(mockContext)).toThrow(ForbiddenException);
+    expect(() => guard.canActivate(mockContext)).toThrow(
+      `User ${user.fullName} need a valid role: [${validRoles.join(',')}]`,
+    );
   });
 });
